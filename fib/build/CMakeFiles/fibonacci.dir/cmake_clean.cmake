c8dd21a1e19924b214f772a51e7f578826521ad0
eriks27 <eriks.zviedrans@gmail.com>
2012-12-16 23:27:12 +0200

    added fib

---
+++ fib/build/CMakeFiles/fibonacci.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/fibonacci.dir/fib.c.o"
+  "fibonacci.pdb"
+  "fibonacci"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/fibonacci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
